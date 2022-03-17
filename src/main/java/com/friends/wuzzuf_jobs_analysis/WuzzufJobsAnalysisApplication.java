3c532eb618780b7993687ad1f94913f0a072413d
ahmad <ahmed.salamony497@gmail.com>
2022-03-17 17:41:35 +0200

    First commit

---
+++ src/main/java/com/friends/wuzzuf_jobs_analysis/WuzzufJobsAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.friends.wuzzuf_jobs_analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuzzufJobsAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WuzzufJobsAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/friends/wuzzuf_jobs_analysis/WuzzufJobsAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friends.wuzzuf_jobs_analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WuzzufJobsAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
